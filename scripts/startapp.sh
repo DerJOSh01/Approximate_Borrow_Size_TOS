60d86a7c3c75cdc30304938ec9cf16e9a71b9f47
gitricko <gitricko@users.noreply.github.com>
2025-10-24 10:16:50 -0700

    fix bugs (#3)

--- scripts/startapp.sh
+++ scripts/startapp.sh
@@ -3,7 +3,7 @@
 # bashrc setup
 if [ ! -f /home/app/.bashrc ]; then
     # echo "python -m venv /tmp/venv" >> /home/app/.bashrc
-    echo "source /tmp/venv/bin/activate" >> /home/app/.bashrc
+    echo "source /home/app/venv/bin/activate" >> /home/app/.bashrc
     echo "cd /codespace" >> /home/app/.bashrc
     # echo "pip install -r requirements.txt" >> /home/app/.bashrc
     echo "python watchlist_launcher_gui.py" >> /home/app/.bashrc
